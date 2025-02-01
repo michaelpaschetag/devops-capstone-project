a09e486f5fce4a4b84be4acb3cc680566c392f6e
Michael Paschetag <mpaschetag@yahoo.com>
2025-02-01 14:53:06 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -28,6 +28,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
+
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
@@ -195,3 +196,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
